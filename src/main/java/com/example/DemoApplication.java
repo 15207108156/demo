c9c9f843b16cf78a7af7ff60ef08edd55c30da48
15207108156 <15207108156@163.com>
2017-10-31 17:22:15 +0800

    修改pom.xml使项目运行，但是由于注解的问题，并不能完美运行

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //启动类
 @SpringBootApplication
+@MapperScan("com.example.mapper")
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
